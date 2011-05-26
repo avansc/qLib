35651419a7887bbf30deb8a862197f82e64ead4c
avansc <avansc@gmail.com>
2011-05-26 14:11:03 -0400

    qUtil initial commit

---
+++ qUtil/src/qComponent.cpp
@@ -0,0 +1,85 @@
+/*
+ *  qIOLib
+ *  qComponent.cpp
+ *
+ *	Copyright (c) 2011, AVS
+ *	All rights reserved.
+ *
+ *	Redistribution and use in source and binary forms, with or without
+ *	modification, are permitted provided that the following conditions are met:
+ *	1.	Redistributions of source code must retain the above copyright
+ *		notice, this list of conditions and the following disclaimer.
+ *	2.	Redistributions in binary form must reproduce the above copyright
+ *		notice, this list of conditions and the following disclaimer in the
+ *		documentation and/or other materials provided with the distribution.
+ *	3.	All advertising materials mentioning features or use of this software
+ *		must display the following acknowledgement:
+ *		This product includes software developed by the AVS.
+ *	4.	Neither the name of the AVS nor the
+ *		names of its contributors may be used to endorse or promote products
+ *		derived from this software without specific prior written permission.
+ *
+ *	THIS SOFTWARE IS PROVIDED BY AVS ''AS IS'' AND ANY
+ *	EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *	DISCLAIMED. IN NO EVENT SHALL AVS BE LIABLE FOR ANY
+ *	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "qComponent.h"
+
+#include <stdlib.h>
+
+using namespace qLib::Util;
+
+qComponent::qComponent()
+:	root(NULL)
+{
+}
+
+qComponent::qComponent(qComponent *root)
+:	root(root)
+{
+}
+
+qComponent::~qComponent()
+{
+	std::map<std::string, qComponent* >::iterator it;
+	
+	for(it = this->comps.begin();it != comps.end();it++)
+		delete it->second;
+}
+
+qComponent *qComponent::getRoot()
+{
+	return this->root;
+}
+
+qComponent *qComponent::getComp(std::string comp)
+{
+	return this->comps[comp];
+}
+
+bool qComponent::addComp(std::string comp, qComponent *it)
+{
+	it->root = this;
+	this->comps[comp] = it;
+	return true;
+}
+
+void qComponent::REGISTER_SCRIPTABLES(qScriptEngine *engine)
+{
+	//engine->RegisterScriptable<qEntity>();
+	REGISTER_CLASS(engine, "qComponent", qComponent);
+	
+	
+	int r = engine->getEngine()->RegisterObjectMethod("qComponent", "qComponent &opAssign(const qComponent &in)", asMETHODPR(qComponent, operator =, (const qComponent&), qComponent&), asCALL_THISCALL); assert( r >= 0 );
+	
+	REGISTER_METHOD(engine, "qComponent", qComponent, "bool addComp(string &comp, qComponent &it)", addComp);
+	REGISTER_METHOD(engine, "qComponent", qComponent, "qComponent &getComp(string &comp)", getComp);
+}
