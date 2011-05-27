d3b0e2bafe4c64b79c6b4c20707db4ac05079d04
avansc <avansc@gmail.com>
2011-05-27 17:28:56 -0400

    Initial commit of qLib usage example. Nothing special

---
+++ qLib_usage_example/qLib_usage_example/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  qLib_usage_example
+//
+//  Created by avansc on 5/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
